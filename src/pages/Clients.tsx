343b11571e742d262df9fccb68802b1c005e3b74
rhymes17 <devrahul1709@gmail.com>
2024-08-03 04:25:50 +0530

    chore: home page and add gsap

---
+++ src/pages/Clients.tsx
@@ -0,0 +1,11 @@
+import SectionLayout from "../components/SectionLayout";
+
+const Clients = () => {
+  return (
+    <SectionLayout title="Our Clients">
+      <div></div>
+    </SectionLayout>
+  );
+};
+
+export default Clients;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,22 @@
-import HeroImage from "../assets/images/quantek 1.png";
+import Clients from "./Clients";
+import HeroSection from "./HeroSection";
 // import Logo from "../assets/images/banner-logo-transparent.ce5bc3f3.png";
 import HeroTitle from "./HeroTitle";
+import Services from "./Services";
+import Workflow from "./Workflow";
 
 const Home = () => {
   return (
     <div className="">
-      {/* Hero Section */}
-      <div className="h-[100vh] relative">
-        <img src={HeroImage} className="h-[100vh] object-cover w-[100vw]" />
-
-        {/* <div className="absolute top-[2.5rem] left-[2.5rem]">
-          <img src={Logo} className="h-[70px]" />
-        </div> */}
-
-        <div className="absolute flex flex-col gap-5 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[70%] ">
-          <h1 className="text-[7rem] text-white text-center tracking-[2.1rem] font-[600]">
-            QUANTEK
-          </h1>
-        </div>
-      </div>
+      <HeroSection />
 
       <HeroTitle />
+
+      <Services />
+
+      <Workflow />
+
+      <Clients />
     </div>
   );
 };
